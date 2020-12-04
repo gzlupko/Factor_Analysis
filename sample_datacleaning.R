9e0c38d69786eafd3426565564bf1691d0f79459
gzlupko <gianzlupko@gmail.com>
2020-12-03 21:22:08 -0500

    first commit; code to convert text to numeric if needed

---
+++ sample_datacleaning.R
@@ -0,0 +1,40 @@
+# sample data cleaning for research survey text items 
+
+
+
+library(readxl) 
+library(openxlsx) 
+library(rio)
+
+sample <- read_excel("sample_responsibility_2.xlsx") 
+
+sample <- as.data.frame(sample)
+
+sample[sample == "Strongly disagree"] <- 0
+sample[sample == "Somewhat disagree"] <- 1
+sample[sample == "Neither agree nor disagree"] <- 2
+sample[sample == "Somewhat agree"] <- 3
+sample[sample == "Strongly agree"] <- 4
+
+
+sample <- sample[-1, ]
+sample <- sample[ , 1:50]
+
+scale <- sample[1:5, 1:50]
+scale <- sapply(scale, as.numeric) 
+str(scale)
+View(scale) 
+
+
+export(scale, "sample_output.xlsx") 
+
+
+
+
+# test EFA model 
+
+sample_EFA_cor <- cor(scale, use = "pairwise.complete.obs") 
+EFA_model <- fa(scale, nfactors = 4) 
+EFA_model$loadings
+
+
